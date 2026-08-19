41a4c418d0e4484d6f4272056db9b489a0e56222
Milagorssgs <cs1079779@gmail.com>
2026-08-19 11:40:35 -0300

    ahora si puto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ import {
 } from 'lucide-react';
 import toast, { Toaster } from 'react-hot-toast';
 
-const API_URL = "https://https://kiosco-backend-db.vercel.app/api";;
+const API_URL = "https://kiosco-backend-db.vercel.app/api";
 
 const playAudio = (type) => {
   const urls = {
